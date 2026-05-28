3a4a145a84b35e1c046c384b7be60314462076d4
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-05-28 07:15:24 +0100

    feat: TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main() -> None:
+    anchorNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(anchorNode)
+
+
+main()
