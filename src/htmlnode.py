06d7148b2f96302048ea22cf04f98e5a0519b18d
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-08-14 10:41:27 +0100

    parentnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HtmlNode:
-    def __init__(self, tag: str | None = None, value: str | None = None, children: object | None = None,
+    def __init__(self, tag: str | None = None, value: str | None = None, children: list[object] | None = None,
                  props: dict[str, str] | None = None):
         self.tag = tag
         self.value = value
@@ -34,3 +34,24 @@ class LeafNode(HtmlNode):
 
     def __repr__(self) -> str:
         return f"HtmlNode(tag={self.tag}, value={self.value}, props={self.props})"
+
+
+class ParentNode(HtmlNode):
+    def __init__(self, tag: str | None, children: list[HtmlNode], props: dict[str, str] | None = None):
+        super().__init__(tag, None, children, props)
+
+
+    def to_html(self) -> str:
+        if self.tag is None or self.tag == "":
+            raise ValueError("ParentNode tag cannot be empty")
+
+        if self.children is None:
+            raise ValueError("ParentNode must have at least 1 child")
+
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
